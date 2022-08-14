63ab5344d7ab41df1eb0a1ccc4650096b882de2f
rhyg <rhysgeary@gmail.com>
2022-08-15 05:49:02 +1000

    Styling updates to add collection sheet.

--- app/screens/collections/components/AddCollectionSheet.tsx
+++ app/screens/collections/components/AddCollectionSheet.tsx
@@ -1,53 +1,32 @@
-import {
-  BottomSheetBackdrop,
-  BottomSheetModal,
-  BottomSheetTextInput,
-} from '@gorhom/bottom-sheet';
-import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { BottomSheetModal, BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import React, { useRef, useState } from 'react';
 import EmojiPicker from 'rn-emoji-keyboard';
 import type { EmojiType } from 'rn-emoji-keyboard/src/types';
 import styled, { useTheme } from 'styled-components/native';
 
 import { Text } from '@app/components/Text';
+import { Button } from '@app/components/buttons/Button';
+import { BottomSheet } from '@app/components/sheets/BottomSheet';
+import { getRandomEmoji } from '@app/lib/getRandomEmoji';
 
 type Props = {
   addCollection: (name: string, collectionIcon: string) => void;
 };
 
-const bottomSheetStyle = { zIndex: 2 };
+const customSnapPoints = ['35%'];
 
 export const AddCollectionSheet = React.forwardRef<BottomSheetModal, Props>(
   ({ addCollection }, ref) => {
     const { colours } = useTheme();
-    const { bottom: bottomInset } = useSafeAreaInsets();
 
     const [inputValue, setInputValue] = useState('');
-    const [collectionIcon, setCollectionIcon] = useState<string>('');
+    const [collectionIcon, setCollectionIcon] = useState<string>(
+      getRandomEmoji(),
+    );
 
     const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
 
     const collectionNameInputRef = useRef<typeof BottomSheetTextInput>();
-
-    /* The bottom sheet is slightly higher on phones with a bottom bar */
-    const snapPoints = useMemo(() => {
-      const snapPoint = bottomInset > 0 ? '45%' : '48%';
-
-      return [snapPoint];
-    }, [bottomInset]);
-
-    /* Renders the darkened backdrop behind the sheet */
-    const renderBackdrop = useCallback(
-      props => (
-        <BottomSheetBackdrop
-          {...props}
-          disappearsOnIndex={-1}
-          appearsOnIndex={1}
-        />
-      ),
-      [],
-    );
 
     const onAddCollectionButtonPress = () => {
       addCollection(inputValue, collectionIcon);
@@ -59,32 +38,29 @@ export const AddCollectionSheet = React.forwardRef<BottomSheetModal, Props>(
 
     return (
       <>
-        <BottomSheetModal
-          index={0}
-          snapPoints={snapPoints}
+        <BottomSheet
+          sheetTitle="New collection"
           ref={ref}
-          enablePanDownToClose={true}
-          style={bottomSheetStyle}
-          backdropComponent={renderBackdrop}>
-          <SheetContainer>
-            <InputContainer isFocused={false}>
-              <TextInput
-                placeholder="Collection name"
-                onChangeText={setInputValue}
-                // @ts-ignore this type is gross, not sure how to fix
-                ref={collectionNameInputRef}
-                placeholderTextColor={colours.grey100}
-              />
-            </InputContainer>
-            <AddCollectionButton onPress={onAddCollectionButtonPress}>
-              <Text color={colours.grey300}>Add collection</Text>
-            </AddCollectionButton>
-            <AddCollectionButton onPress={() => setEmojiPickerOpen(true)}>
-              <Text color={colours.grey300}>Pick icon</Text>
-            </AddCollectionButton>
-            <Text>{collectionIcon}</Text>
-          </SheetContainer>
-        </BottomSheetModal>
+          customSnapPoints={customSnapPoints}>
+          <InputContainer isFocused={false}>
+            <TextInput
+              placeholder="Collection name"
+              onChangeText={setInputValue}
+              // @ts-ignore this type is gross, not sure how to fix
+              ref={collectionNameInputRef}
+              placeholderTextColor={colours.grey100}
+            />
+          </InputContainer>
+
+          <ChooseIconButton onPress={() => setEmojiPickerOpen(true)}>
+            <Text fontSize="xxxl">{collectionIcon}</Text>
+          </ChooseIconButton>
+          <Button
+            title="Create collection"
+            onPress={onAddCollectionButtonPress}
+            containerStyle={{ marginTop: 'auto' }}
+          />
+        </BottomSheet>
         <EmojiPicker
           onEmojiSelected={handlePick}
           open={emojiPickerOpen}
@@ -94,11 +70,6 @@ export const AddCollectionSheet = React.forwardRef<BottomSheetModal, Props>(
     );
   },
 );
-
-const SheetContainer = styled.View`
-  flex: 1;
-  padding: 20px;
-`;
 
 const InputContainer = styled.View<{ isFocused: boolean }>`
   border-radius: 10px;
@@ -111,6 +82,13 @@ const TextInput = styled(BottomSheetTextInput)`
   padding: 10px;
 `;
 
-const AddCollectionButton = styled.TouchableOpacity`
-  margin-top: 10px;
+const ChooseIconButton = styled.TouchableOpacity`
+  background-color: ${({ theme }) => theme.colours.grey000};
+  border-radius: 50px;
+  height: 70px;
+  width: 70px;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  margin-top: 20px;
 `;
