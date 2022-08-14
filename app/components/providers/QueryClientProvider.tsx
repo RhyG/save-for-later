5744b1883fc086013724c3e0ac4670e2974e9e5d
rhyg <rhysgeary@gmail.com>
2022-08-14 10:08:47 +1000

    Added app state listener to query client provider.

--- app/components/providers/QueryClientProvider.tsx
+++ app/components/providers/QueryClientProvider.tsx
@@ -1,9 +1,10 @@
 import {
   QueryClient,
-  QueryClientProvider as RCQueryClientProvider,
+  QueryClientProvider as _QueryClientProvider,
 } from '@tanstack/react-query';
-import React from 'react';
-import { PropsWithChildren } from 'react';
+import { focusManager } from '@tanstack/react-query';
+import React, { PropsWithChildren, useEffect } from 'react';
+import { AppState } from 'react-native';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 0 } },
@@ -11,6 +12,20 @@ const queryClient = new QueryClient({
 
 export const QueryClientProvider = ({
   children,
-}: PropsWithChildren<Record<string, unknown>>) => (
-  <RCQueryClientProvider client={queryClient}>{children}</RCQueryClientProvider>
-);
+}: PropsWithChildren<Record<string, unknown>>) => {
+  useEffect(() => {
+    focusManager.setEventListener(handleFocus => {
+      const subscription = AppState.addEventListener('change', state => {
+        handleFocus(state === 'active');
+      });
+
+      return () => {
+        subscription.remove();
+      };
+    });
+  }, []);
+
+  return (
+    <_QueryClientProvider client={queryClient}>{children}</_QueryClientProvider>
+  );
+};
