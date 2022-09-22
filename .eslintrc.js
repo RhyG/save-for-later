614c6b7770a7f48aecec5f6cdbce66af9e75cf15
rhyg <rhysgeary@gmail.com>
2022-09-22 17:38:23 +1000

    Added import rule to ESLint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  plugins: ['detox', 'jasmine', '@typescript-eslint', 'import'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        'no-undef': 'off',
+  parserOptions: {
+    sourceType: 'module',
+    jsx: true,
+    project: './tsconfig.json',
+    createDefaultProgram: true,
+  },
+  env: {
+    browser: true,
+    jasmine: true,
+    'detox/detox': true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'no-catch-shadow': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    // 'no-cycle': 2,
+    radix: 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.android.tsx', '.ios.tsx', '.android.js', '.ios.js'],
       },
+      'babel-module': {},
     },
-  ],
+    react: {
+      version: 'detect',
+    },
+  },
 };
