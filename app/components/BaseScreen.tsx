b0d992e023b89fcee813ec3366c962ed11386bb1
rhyg <rhysgeary@gmail.com>
2022-11-02 05:50:00 +1000

    Added scrolling and safe area to BaseScreen component.

--- app/components/BaseScreen.tsx
+++ app/components/BaseScreen.tsx
@@ -3,14 +3,36 @@ import styled from 'styled-components/native';
 
 type Props = React.PropsWithChildren<{
   noPadding?: boolean;
+  noScroll?: boolean;
 }>;
 
-export const BaseScreen = ({ children, noPadding }: Props): JSX.Element => {
-  return <BaseScreenContainer noPadding={noPadding}>{children}</BaseScreenContainer>;
+export const BaseScreen = ({ children, noPadding, noScroll }: Props): JSX.Element => {
+  if (noScroll) {
+    return (
+      <SafeAreaContainer>
+        <BaseScreenContainer noPadding={noPadding}>{children}</BaseScreenContainer>
+      </SafeAreaContainer>
+    );
+  }
+
+  return (
+    <SafeAreaContainer>
+      <ScrollViewContainer contentContainerStyle={scrollViewContentContainerStyle}>{children}</ScrollViewContainer>
+    </SafeAreaContainer>
+  );
 };
+
+const SafeAreaContainer = styled.SafeAreaView`
+  flex: 1;
+`;
 
 const BaseScreenContainer = styled.View<{ noPadding?: boolean }>`
   flex: 1;
   padding: ${props => (props.noPadding ? 0 : 10)}px;
-  /* background-color: #f9fcff; */
 `;
+
+const ScrollViewContainer = styled.ScrollView<{ noPadding?: boolean }>`
+  padding: ${props => (props.noPadding ? 0 : 10)}px;
+`;
+
+const scrollViewContentContainerStyle = { flex: 1 };
