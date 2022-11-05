73d95c19951b00db849826701a6b7b68f8166712
rhyg <rhysgeary@gmail.com>
2022-11-05 10:05:44 +1000

    Minor update to bookmark generation function.

--- app/lib/bookmarks.ts
+++ app/lib/bookmarks.ts
@@ -1,13 +1,9 @@
 import { IBookmark } from '@app/types';
 
 export const generateBookmarkWithPreviewDetails = (
-  details: any,
-  user_id?: string,
-): Omit<IBookmark, 'id'> | undefined => {
-  if (!user_id) {
-    return;
-  }
-
+  details: any, // TODO Correctly type this
+  // user_id?: string,
+): Omit<IBookmark, 'id' | 'user_id'> | undefined => {
   const { title, description, url, images, siteName } = details;
 
   const bookmark = {
@@ -17,7 +13,6 @@ export const generateBookmarkWithPreviewDetails = (
     preview_image: images[0] ?? '',
     number_of_opens: 0,
     collections: [],
-    user_id,
   };
 
   return bookmark;
