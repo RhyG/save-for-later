3be4346a81a0a6386455b386857d4f7c7dd01e01
rhyg <rhysgeary@gmail.com>
2022-11-05 10:05:55 +1000

    Added URL validation util.

--- app/lib/validation.ts
+++ app/lib/validation.ts
@@ -1,13 +1,17 @@
-export const validateEmail = (email: string) => {
-  const isEmailValid = String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    );
+const URL_REGEX = new RegExp(
+  /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$$/,
+);
 
+export const validateEmail = (email: string) => {
   return String(email)
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     );
+};
+
+export const validateURL = (url: string) => {
+  const isValidURL = URL_REGEX.test(url);
+
+  return isValidURL;
 };
