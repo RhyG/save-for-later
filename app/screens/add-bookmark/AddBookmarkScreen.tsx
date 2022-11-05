fdf120958be94d18fc0888d446e4b1ced95d360c
rhyg <rhysgeary@gmail.com>
2022-11-05 10:06:30 +1000

    WIP work for manual bookmark creation.

--- app/screens/add-bookmark/AddBookmarkScreen.tsx
+++ app/screens/add-bookmark/AddBookmarkScreen.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { useTheme } from 'styled-components/native';
 
+import { BookmarksAPI } from '@app/api/bookmarks';
+import { useShareIntent } from '@app/hooks/useShareIntent';
+import { IBookmark } from '@app/types';
+
 export const AddBookmarkScreen = () => {
   const { colours } = useTheme();
+
+  const addNewBookmark = async (bookmark: Omit<IBookmark, 'id'>) => {
+    try {
+      await BookmarksAPI.addBookmark(bookmark);
+    } catch (error) {
+      // TODO update with more graceful errors when implemented
+      console.error(error);
+    }
+  };
+
+  // const { newBookmarkDetails, loadingNewBookmarkDetails } = useShareIntent(addNewBookmark);
 
   return <></>;
 };
--- app/screens/add-bookmark/EnterManualBookmarkScreen.tsx
+++ app/screens/add-bookmark/EnterManualBookmarkScreen.tsx
@@ -1,18 +1,58 @@
-import React from 'react';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { getLinkPreview } from 'link-preview-js';
+import React, { useRef, useState } from 'react';
+import { TextInput as RNTextInput } from 'react-native';
 
 import { BaseScreen } from '@app/components/BaseScreen';
 import { Text } from '@app/components/Text';
+import { TextInput } from '@app/components/TextInput';
 import { Button } from '@app/components/buttons/Button';
+import { useHeaderTitle } from '@app/hooks/useHeaderTitle';
+import { generateBookmarkWithPreviewDetails } from '@app/lib/bookmarks';
+import { validateURL } from '@app/lib/validation';
+import { RootStackParamList } from '@app/navigation/types';
+import { IBookmark } from '@app/types';
 
-export const EnterManualBookmarkScreen = ({ navigation }) => {
+type Props = NativeStackScreenProps<RootStackParamList, 'EnterManualBookmarkScreen'>;
+
+export const EnterManualBookmarkScreen = ({ navigation }: Props) => {
+  useHeaderTitle('Save a bookmark');
+
+  const [inputValue, setInputValue] = useState('');
+
+  const onSearchBookmarkPress = async () => {
+    if (!validateURL(inputValue)) {
+      return;
+    }
+
+    const preview = (await getLinkPreview(inputValue)) as unknown as IBookmark;
+
+    if (!preview) {
+      // TODO Inform user no preview found - maybe ask if they want to save anyway?
+      return;
+    }
+
+    const newBookmark = generateBookmarkWithPreviewDetails(preview);
+
+    if (!newBookmark) {
+      // TODO Inform user error occurred
+      return;
+    }
+
+    navigation.navigate('AddManualBookmarkScreen', { bookmark: newBookmark });
+  };
+
   return (
     <BaseScreen noScroll>
-      <Text>ENTER URL</Text>
-      <Button
-        title="Search"
-        onPress={() => navigation.navigate('AddManualBookmarkScreen')}
-        containerStyle={{ marginTop: 'auto' }}
+      <Text marginBottom={2}>Enter the URL you want to save</Text>
+      <TextInput
+        placeholder="https://example.com"
+        onChangeText={setInputValue}
+        autoCapitalize="none"
+        autoCorrect={false}
+        autoFocus={true}
       />
+      <Button title="Search" onPress={onSearchBookmarkPress} containerStyle={{ marginTop: 'auto' }} />
     </BaseScreen>
   );
 };
